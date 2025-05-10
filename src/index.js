fc436962e2196d529929e17c7c118964192b5bc8
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-05-10 18:03:33 +0600

    setup server and config the env with dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello everyone, it's me your bro");
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/hello", (req, res) => {
+  res.json({
+    wow: "nice",
+  });
+});
+
+const setupAndStartServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+};
+
+setupAndStartServer();
