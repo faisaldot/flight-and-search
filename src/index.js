ffdbd5b7b44b99d4c9eb962a566386d3065ae867
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-05-11 14:41:52 +0600

    added body-parser

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const { PORT } = require("./config/serverConfig");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.get("/hello", (req, res) => {
-  res.json({
-    wow: "nice",
-  });
-});
-
 const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
   });
